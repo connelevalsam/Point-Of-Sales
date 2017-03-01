a3d03591c9f1f154d9b0769144d280bd950f8dfd
ConnelBLAZE <connelblaze@gmail.com>
2017-03-01 14:28:04 +0100

    final commit

--- PointOfSales.java
+++ PointOfSales.java
@@ -4,17 +4,16 @@ import java.util.Collection;
 
 import java.util.*;
 /*
-*											   *
+*					       *
 * Program for part of Point-Of-Sales system    *
-* Program developed by Gedoni Ani              *
-* Program contributor cb 					   *
+* Program developed by Connel Evalsam Asikong  *
 * Program done to handle transactions for a    *
 * local hardware store and need to be able to: *
 * 1. Display a menu for the user after login.  *
-* 2. Validate user input .					   *
-* 3. Keep track of a user's account 		   *
-* (Starting balance $50.00). 				   *
-* 4. Keep track of a user's purchases.		   *
+* 2. Validate user input .		       *
+* 3. Keep track of a user's account 	       *
+* (Starting balance $50.00). 		       *
+* 4. Keep track of a user's purchases.	       *
 * 5. Continue until the user chooses to exit.  *
 *											   *
 */
